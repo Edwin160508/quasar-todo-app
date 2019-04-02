96e7c8030ebaf6e50373b87a0033ad7760f1f6a2
Edwin Lima <edwin.limaa@gmail.com>
2019-04-02 09:01:08 -0300

    Incluindo QInput para criação de campos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding>
+    <div class="row">
+      <q-input v-model="text" placeholder="Add new task" class="col" />
+      <q-btn color="primary" size="sm" label="Add"/>
+    </div>
+    <div class="row">
+        <q-list highlight>
+            <q-list-header>Recent chats</q-list-header>
+            <q-item>
+              <q-item-side>
+                <q-item-tile avatar>
+                  <img src="statics/boy-avatar.png">
+                </q-item-tile>
+              </q-item-side>
+              <q-item-main label="John Doe" />
+              <q-item-side right>
+                <q-item-tile icon="chat_bubble" color="green" />
+              </q-item-side>
+            </q-item>
+            <q-item>
+              <q-item-side avatar="statics/linux-avatar.png" />
+              <q-item-main label="Jim Doe" />
+              <q-item-side right icon="chat_bubble" />
+            </q-item>
+            <q-item-separator />
+            <q-list-header>Previous chats</q-list-header>
+            <q-item>
+              <q-item-side avatar="statics/guy-avatar.png" />
+              <q-item-main label="Jack Doe" />
+            </q-item>
+          </q-list>
+    </div>
   </q-page>
 </template>
 
