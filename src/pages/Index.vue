f5a6e36e829aa3d47121662889bdc1c859a6506e
Edwin Pereira Oliveira Lima <eplima1@stefanini.com>
2019-04-02 14:57:45 -0300

    Retirando previous list.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,12 +23,6 @@
               <q-item-main label="Jim Doe" />
               <q-item-side right icon="chat_bubble" />
             </q-item>
-            <q-item-separator />
-            <q-list-header>Previous chats</q-list-header>
-            <q-item>
-              <q-item-side avatar="statics/guy-avatar.png" />
-              <q-item-main label="Jack Doe" />
-            </q-item>
           </q-list>
     </div>
   </q-page>
