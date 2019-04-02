506f1cf6cc6b9368d8437b8fc3d206e32c52ab40
Edwin Pereira Oliveira Lima <eplima1@stefanini.com>
2019-04-02 17:35:22 -0300

    Passando tastks para lista done.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,27 @@
 <template>
   <q-page padding>
-    <div class="row">
+    <div class="row q-mb-lg">
       <q-input v-model="newTask" placeholder="Add new task" class="col" @keyup.enter="addTask" />
       <q-btn color="primary" size="sm" label="Add" @click.native="addTask" />
     </div>
-    <div class="row">
+    <div class="row q-mb-lg">
         <q-list highlight class="col">
             <q-list-header>Tasks</q-list-header>
             <q-item v-for="(task, index) in tasks" :key="index">
               <q-item-main :label="task" />
-              <q-item-side right icon="check" color="green" />
+              <q-item-side right icon="check" color="green" @click.native="moveToDone(index)" />
             </q-item>
           </q-list>
     </div>
+    <div class="row">
+      <q-list highlight class="col">
+          <q-list-header>Done</q-list-header>
+          <q-item v-for="(task, index) in done" :key="index">
+            <q-item-main :label="task" />
+            <q-item-side right icon="check" color="green" />
+          </q-item>
+        </q-list>
+  </div>
   </q-page>
 </template>
 
@@ -23,7 +32,7 @@
 export default {
   name: 'PageIndex',
   data () {
-    return { tasks: [], newTask: '' }
+    return { tasks: [], done: [], newTask: '' }
   },
   methods: {
     addTask () {
@@ -31,6 +40,10 @@ export default {
         this.tasks.push(this.newTask)
       }
       this.newTask = ''
+    },
+    moveToDone (index) {
+      this.done.push(this.tasks[index])
+      this.tasks.splice(index, 1)
     }
   }
 }
