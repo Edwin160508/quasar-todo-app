3b62f2e54af3ee3a1ea991ecd95f906081f57207
Edwin Pereira Oliveira Lima <eplima1@stefanini.com>
2019-04-02 16:12:10 -0300

    Não permitir adicionar tasks vazias

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,7 +27,9 @@ export default {
   },
   methods: {
     addTask () {
-      this.tasks.push(this.newTask)
+      if (this.newTask !== '') {
+        this.tasks.push(this.newTask)
+      }
       this.newTask = ''
     }
   }
